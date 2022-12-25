ae29f4e29ce51aa10f89c07b87823ae0782549dc
mthelf <michaelthelf@gmail.com>
2022-12-25 02:40:05 -0500

    finished preloader animation. worked on some stlying

---
+++ app.js
@@ -0,0 +1,16 @@
+// document.addEventListener('DOMContentLoaded', (e) => {
+
+//     setTimeout(() => {
+
+//         document.getElementById("splashscreen").style.opacity = 0;
+//     }, 4000);
+// } )
+
+document.addEventListener('DOMContentLoaded', (e) => {
+
+    setTimeout(() => {
+
+        document.getElementById("splashscreen").classList.add('hidden');
+    }, 3000);
+} )
+
