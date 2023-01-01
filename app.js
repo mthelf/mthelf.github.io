12e94615a21a74a2c40d9e19062d3000fd874a24
mthelf <michaelthelf@gmail.com>
2023-01-01 03:47:34 -0500

    started projects section

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-// document.addEventListener('DOMContentLoaded', (e) => {
-
-//     setTimeout(() => {
-
-//         document.getElementById("splashscreen").style.opacity = 0;
-//     }, 4000);
-// } )
-
 document.addEventListener('DOMContentLoaded', (e) => {
 
     setTimeout(() => {
@@ -14,3 +6,29 @@ document.addEventListener('DOMContentLoaded', (e) => {
     }, 2000);
 } )
 
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
